34cba45da4fa84ac2bb477d242ada9ad37a9e58e
Meri Alikhanyan <108768313+alikhanyyan@users.noreply.github.com>
2024-01-20 22:12:26 +0400

    Add files via upload

---
+++ src/main/java/org/example/MenuCodes.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public enum MenuCodes {
+    EXIT("0"),
+    UPDATE_BOOK_DETAILS("1"),
+    LIST_BOOKS_BY_GENRE("2"),
+    LIST_BOOKS_BY_AUTHOR("3"),
+    UPDATE_CUSTOMER_INFORMATION("4"),
+    VIEW_CUSTOMER_PURCHASE_HISTORY("5"),
+    PROCESS_NEW_SALE("6"),
+    CALCULATE_TOTAL_REVENUE_BY_GENRE("7"),
+    GENERATE_SALES_REPORT("8"),
+    GENERATE_REVENUE_REPORT_BY_GENRE("9");
+
+    MenuCodes(String strCode) {
+    }
+}
+
